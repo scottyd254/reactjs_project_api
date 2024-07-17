83b19e64abe08073ddc69a7fe9be0efe5bcfb0f6
scottyd254 <zealousegg.picoworkers@gmail.com>
2024-07-17 23:07:32 +0300

    changed url for joblistings to https://scottyd254.pythonanywhere.com/

--- react_frontend_tutorial/src/components/JobListings.jsx
+++ react_frontend_tutorial/src/components/JobListings.jsx
@@ -1,20 +1,20 @@
-
-import JobListing from "./JobListing"
+import JobListing from "./JobListing";
 import { useState, useEffect } from "react";
 
-
 // eslint-disable-next-line react/prop-types
-const JobListings = ({isHome = false}) => {
+const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
-  
+
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
-        const res = await fetch('http://localhost:8000/api/get/jobs');
-        const data = await res.json()
+        const res = await fetch(
+          "https://scottyd254.pythonanywhere.com/api/get/jobs"
+        );
+        const data = await res.json();
         setJobs(data);
       } catch (error) {
-        console.log('Error fetching data', error)
+        console.log("Error fetching data", error);
       }
     };
 
@@ -22,25 +22,19 @@ const JobListings = ({isHome = false}) => {
   }, []);
   return (
     <section className="bg-blue-50 px-4 py-10">
-    <div className="container-xl lg:container m-auto">
-      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        { isHome ? "Recent Jobs" : "Browse Jobs"}
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-       {jobs.map((job) => (
-           // eslint-disable-next-line react/jsx-key
-           <JobListing key = {job.id} job = {job} />
-           
-       ))}
-        
-        
-        
-        
-       
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => (
+            // eslint-disable-next-line react/jsx-key
+            <JobListing key={job.id} job={job} />
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default JobListings
+export default JobListings;
